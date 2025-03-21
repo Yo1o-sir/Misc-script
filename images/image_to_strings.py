db3b71e6545619f15d8cefbfc59628d3dcdf7301
Yo1o <2406213396@qq.com>
2025-03-21 20:57:59 +0800

    Add files via upload

--- images/image_to_strings.py
+++ images/image_to_strings.py
@@ -1,1 +1,13 @@
+#这里建议使用Linux环境运行
+#提前准备安装sudo apt install tesseract-ocr
+from PIL import Image
+import pytesseract
 
+image_path='misc3.png'
+image=Image.open(image_path)
+
+
+test=pytesseract.image_to_string(image,lang='eng')
+
+print("the content in the photo:")
+print(test)
